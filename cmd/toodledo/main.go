5492babb22482c6d1fd4ac39ca719d44ab2053e3
alswl <alswlx@gmail.com>
2021-08-21 00:25:40 +0800

    feat: folder get works

--- cmd/toodledo/main.go
+++ cmd/toodledo/main.go
@@ -1,16 +1,40 @@
 package main
 
 import (
-	"github.com/alswl/go-toodledo/pkg/toodledo"
+	"fmt"
+	"github.com/alswl/go-toodledo/pkg/client"
+	"github.com/alswl/go-toodledo/pkg/client/folder"
+	"github.com/go-openapi/runtime"
+	"github.com/go-openapi/strfmt"
 	log "github.com/sirupsen/logrus"
 	"os"
 )
+
+type Auth struct {
+	accessToken string
+}
+
+func NewAuth(accessToken string) *Auth {
+	return &Auth{accessToken: accessToken}
+}
+
+func (a *Auth) AuthenticateRequest(request runtime.ClientRequest, registry strfmt.Registry) error {
+	request.SetQueryParam("access_token", a.accessToken)
+	return nil
+}
 
 func main() {
 	accessToken := os.Getenv("TOODLEDO_ACCESS_TOKEN")
 	if accessToken == "" {
 		log.Fatal("Unauthorized: No TOODLEDO_ACCESS_TOKEN present")
 	}
+	auth := NewAuth(accessToken)
 
-	toodledo.NewClient(accessToken)
+	cli := client.NewHTTPClient(strfmt.NewFormats())
+	fs, err := cli.Folder.GetFoldersGetPhp(folder.NewGetFoldersGetPhpParams(), auth)
+	if err != nil {
+		log.Error(err)
+		return
+	}
+	fmt.Printf("%v", fs)
 }
--- go.mod
+++ go.mod
@@ -4,15 +4,18 @@ go 1.12
 
 require (
 	github.com/elazarl/goproxy v0.0.0-20200809112317-0581fc3aee2d // indirect
+	github.com/go-openapi/errors v0.20.0
+	github.com/go-openapi/runtime v0.19.30
+	github.com/go-openapi/strfmt v0.20.1
+	github.com/go-openapi/swag v0.19.15
+	github.com/go-openapi/validate v0.20.2
 	github.com/go-playground/universal-translator v0.17.0 // indirect
 	github.com/go-playground/validator v9.31.0+incompatible
 	github.com/leodido/go-urn v1.2.0 // indirect
 	github.com/parnurzeal/gorequest v0.2.16
-	github.com/pkg/errors v0.9.1 // indirect
 	github.com/sirupsen/logrus v1.6.0
 	github.com/smartystreets/goconvey v1.6.4 // indirect
-	github.com/stretchr/testify v1.6.1
-	golang.org/x/net v0.0.0-20200904194848-62affa334b73 // indirect
+	github.com/stretchr/testify v1.7.0
 	gopkg.in/go-playground/assert.v1 v1.2.1 // indirect
 	moul.io/http2curl v1.0.0 // indirect
 )
