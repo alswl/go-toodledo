504d8992f89a40792d6478b18c4659bef2548f4e
alswl <alswlx@gmail.com>
2021-10-27 00:19:02 +0800

    feat: root invoke child command

--- cmd/toodledo/commands/contexts/contexts.go
+++ cmd/toodledo/commands/contexts/contexts.go
@@ -5,6 +5,7 @@ import "github.com/spf13/cobra"
 var ContextCmd = &cobra.Command{
 	Use:   "context",
 	Short: "Manage toodledo contexts",
+	Run:   ListCmd.Run,
 }
 
 func init() {
--- cmd/toodledo/commands/folders/folders.go
+++ cmd/toodledo/commands/folders/folders.go
@@ -1,10 +1,13 @@
 package folders
 
-import "github.com/spf13/cobra"
+import (
+	"github.com/spf13/cobra"
+)
 
 var FolderCmd = &cobra.Command{
 	Use:   "folder",
 	Short: "Manage toodledo folders",
+	Run:   ListCmd.Run,
 }
 
 func init() {
