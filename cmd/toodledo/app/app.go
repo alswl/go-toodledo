fb5d359b1e71a81e21576981a87cd574fe76449e
alswl <alswlx@gmail.com>
2022-06-08 23:48:48 +0800

    refactor: svc in app

--- cmd/toodledo/app/app.go
+++ cmd/toodledo/app/app.go
@@ -2,12 +2,20 @@ package app
 
 import (
 	"github.com/alswl/go-toodledo/pkg/models"
+	"github.com/alswl/go-toodledo/pkg/services"
 )
 
 type ToodledoCliApp struct {
 	CurrentUser *models.Account
+
+	AccountSvc     services.AccountService
+	TaskSvc        services.TaskService
+	FolderSvc      services.FolderService
+	ContextSvc     services.ContextService
+	GoalSvc        services.GoalService
+	SavedSearchSvc services.SavedSearchService
 }
 
-func NewToodledoCliApp(currentUser *models.Account) *ToodledoCliApp {
-	return &ToodledoCliApp{CurrentUser: currentUser}
+func NewToodledoCliApp(currentUser *models.Account, accountSvc services.AccountService, taskSvc services.TaskService, folderSvc services.FolderService, contextSvc services.ContextService, goalSvc services.GoalService, savedSearchSvc services.SavedSearchService) *ToodledoCliApp {
+	return &ToodledoCliApp{CurrentUser: currentUser, AccountSvc: accountSvc, TaskSvc: taskSvc, FolderSvc: folderSvc, ContextSvc: contextSvc, GoalSvc: goalSvc, SavedSearchSvc: savedSearchSvc}
 }
