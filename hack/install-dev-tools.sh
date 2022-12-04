13e613f2233018b64717b0397aa3369390b9000b
Jingchao <alswlx@gmail.com>
2022-12-04 14:42:42 +0800

    chore: build and ci (#6)
    * chore: build and ci
    
    * chore: install golangci-lint

---
+++ hack/install-dev-tools.sh
@@ -0,0 +1,7 @@
+#!/usr/bin/env bash
+
+# install golangci-lint
+if ! command -v golangci-lint &> /dev/null; then
+  echo "Installing golangci-lint"
+  curl -sSfL https://raw.githubusercontent.com/golangci/golangci-lint/master/install.sh | sh -s -- -b $(go env GOPATH)/bin v1.50.1
+fi
