3a898ab384ca6906b16426d6abe81f743d7c118e
Jingchao <alswlx@gmail.com>
2022-12-19 23:59:14 +0800

    test: fix viewport failure test

--- pkg/ui/detail/detail_test.go
+++ pkg/ui/detail/detail_test.go
@@ -20,27 +20,48 @@ func TestView(t *testing.T) {
 	m := New(task)
 	m.Resize(400, 20)
 	view := m.View()
-	// println(view)
 	assert.Equal(t,
-		`┌───────────────────────────────────────┐
-│Completed:          [ ]                │
-│ID:                 0                  │
-│Title:              test               │
-│Context:            c                  │
-│Folder:             f                  │
-│Goal:               g                  │
-│Status:             None               │
-│Due:                                   │
-│Repeat:                                │
-│Priority:           Low                │
-│Repeat:                                │
-│Length:                                │
-│Timer:                                 │
-│Tag:                                   │
-│Star:               false              │
-│Note:               nnn                │
-│yyy                                    │
-└───────────────────────────────────────┘`,
+		`┌────────────────────────────────────────────────────────────────────┐
+│Link:               https://www.toodledo.com/tasks/index.php?#task_0│
+│Completed:          [ ]                                             │
+│ID:                 0                                               │
+│Title:              test                                            │
+│Context:            c                                               │
+│Folder:             f                                               │
+│Goal:               g                                               │
+│Status:             None                                            │
+│Due:                                                                │
+│Repeat:                                                             │
+│Priority:           Low                                             │
+│Repeat:                                                             │
+│Length:                                                             │
+│Timer:                                                              │
+│Tag:                                                                │
+│Star:               false                                           │
+│Note:               nnn                                             │
+└────────────────────────────────────────────────────────────────────┘`,
+		view,
+	)
+}
+
+func TestViewMinimal(t *testing.T) {
+	task := models.RichTask{
+		Task: models.Task{
+			Title: "test",
+			Note:  "nnn\nyyy",
+		},
+		TheContext: &models.Context{Name: "c"},
+		TheFolder:  &models.Folder{Name: "f"},
+		TheGoal:    &models.Goal{Name: "g"},
+	}
+	m := New(task)
+	m.Resize(400, 5)
+	view := m.View()
+	assert.Equal(t,
+		`┌────────────────────────────────────────────────────────────────────┐
+│Link:               https://www.toodledo.com/tasks/index.php?#task_0│
+│Completed:          [ ]                                             │
+└────────────────────────────────────────────────────────────────────┘`,
 		view,
 	)
 }
